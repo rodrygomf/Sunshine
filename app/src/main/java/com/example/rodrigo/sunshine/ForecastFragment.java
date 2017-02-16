9e4dcb84f051000e9f153e79b2d1fb70db3498dd
Rodrigo Figueiredo <rodrigo.figueiredo@candeias.tur.br>
2017-02-16 13:19:41 -0200

    Parte - 03 - 01

--- app/src/main/java/com/example/rodrigo/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/rodrigo/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.rodrigo.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -59,6 +62,16 @@ public class ForecastFragment extends Fragment {
         //Pega a referência do List View e anexa ao Adapter
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String forecast = mForecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();;
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+            }
+        });
 
         FetchWeatherTask weatherTask = new FetchWeatherTask();
         weatherTask.execute("Curitiba");
